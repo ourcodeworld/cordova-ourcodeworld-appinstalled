8df1a20a1abf04ed71230c8f0a3060671c79f3b1
Carlos Delgado <sdkcarlos@outlook.com>
2016-06-21 11:55:19 +0200

    Second commit

--- src/android/OurCodeWorldpreventscreenshots.java
+++ src/android/OurCodeWorldpreventscreenshots.java
@@ -14,17 +14,37 @@ public class OurCodeWorldappinstalled extends CordovaPlugin {
         if (ACTION.equals(action)) {
             cordova.getActivity().runOnUiThread(new Runnable() {
                 public void run() {
-                    PluginResult result = new PluginResult(PluginResult.Status.OK, "exists");
-                    result.setKeepCallback(true);
-                    callbacks.sendPluginResult(result);
+                    boolean installed = appInstalledOrNot("com.Ch.Example.pack");
+                    if(installed) {
+                        PluginResult result = new PluginResult(PluginResult.Status.OK, "exists");
+                        result.setKeepCallback(true);
+                        callbacks.sendPluginResult(result);
+                    } else {
+                        PluginResult result = new PluginResult(PluginResult.Status.OK, "doesnt_exists");
+                        result.setKeepCallback(true);
+                        callbacks.sendPluginResult(result);
+                    }
                 }
             });
         }
-
 
         PluginResult pluginResult = new  PluginResult(PluginResult.Status.NO_RESULT);
         pluginResult.setKeepCallback(true); // Keep callback
 
         return true;
+    }
+
+    private boolean appInstalledOrNot(String uri) {
+        PackageManager pm = getPackageManager();
+        boolean app_installed;
+        try {
+            pm.getPackageInfo(uri, PackageManager.GET_ACTIVITIES);
+            app_installed = true;
+        }
+        catch (PackageManager.NameNotFoundException e) {
+            app_installed = false;
+        }
+
+        return app_installed;
     }
 }
